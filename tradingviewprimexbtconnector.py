454437f32ba5244100f1850098fb60d1afbb5ab1
wsdlwizard <nicholasgflower@hotmail.com>
2024-08-04 10:18:29 +0100

    more updates
    Connector from tradingview to primexbt

--- tradingviewprimexbtconnector.py
+++ tradingviewprimexbtconnector.py
@@ -20,5 +20,26 @@ class PrimeXBTConn(tk.Tk):
         super().__init__(*args, **kwargs)
         self.title("TradingView Connector Basic Model by patreon.com//wsdlwizard")
         self.geometry("800x600+2718+53")
+        self.process_name_to_find = "Firefox"
+        self.move_window_to_top_left(self.process_name_to_find)
     def delete_back(self):
         print("Closing primexbtmanager")
+    def move_window_to_top_left(self,process_name):
+        try:
+            # Get all windows matching the process name
+            windows = gw.getWindowsWithTitle(process_name)
+
+            if windows:
+                # Assuming there's only one window with the given process name
+                target_window = windows[0]
+                target_window.moveTo(0, 0)  # Move to top-left corner
+                print(f"Window Dimentions are {target_window.width} by {target_window.height}")
+                target_window.size = (2666,1715)
+                print(f"Moved '{target_window.title}' to the top-left corner.")
+            else:
+                print(f"No window found with process name '{process_name}'.")
+        except Exception as e:
+            print(f"Error: {e}")
+    def move_window(self,hwnd, x, y, width, height):
+        # Offset the x position by 7 (adjust as needed)
+        win32gui.MoveWindow(hwnd, x - 7, y, width, height, True)
